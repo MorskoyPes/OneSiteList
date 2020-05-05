b4a871636e392711c9046c341bed851ee9d708a9
MorskoyPes <kaktyc_real_ii@mail.ru>
2020-05-05 20:04:50 +0300

    Fourth version

--- index.php
+++ index.php
@@ -5,204 +5,212 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>Tessellate by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="stylesheet" href="assets/css/scroll.css" />
-		<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400&display=swap" rel="stylesheet">
-	</head>
-	<body class="is-preload">
+<head>
+    <title>AwesomeAds Media</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" href="assets/css/main.css"/>
+    <link rel="stylesheet" href="assets/css/scroll.css"/>
+    <link rel="stylesheet" href="assets/css/extended.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400&display=swap" rel="stylesheet">
+</head>
+<body class="is-preload">
 
-		<!-- Header -->
-			<section id="header" class="dark">
-				<header>
-					<h1> Reach your clients  with </h1>
-					<p>AwesomeAds Media</p>
-				</header>
-				<footer>
-					<!-- <a href="#first" class="button scrolly">
-
-  <div class="chevron"></div>
+<!-- Header -->
+<section id="header" class="dark">
+    <header>
+        <h1>Reach your clients with</h1>
+        <p>AwesomeAds Media</p>
+    </header>
+    <footer>
+        <!-- <a href="#first" class="button scrolly">
+<div class="chevron"></div>
 </div></a> -->
-				</footer>
-			</section>
+    </footer>
+</section>
 
-		<!-- First -->
+<!-- First -->
+<section id="first" class="dark">
+    <header>
+        <h1>Helping Leading<br>Brands Scale</h1>
+        <p>AwesomeAds is a diverse team of best mediabiyers, performance marketing and just a creative guys focused on
+            helping brands scale revenue and bring their unique vision to life.</p>
+        <p>So we may work with any businnes, online or offline - not even matter. Do you want to sell your handmade toys
+            or a Large Hadron Collider? Just ping us and we'll craft tailored growth strategies and help our clients
+            execute them to drive growth.</p>
+    </header>
+    <footer>
+        <!-- <a href="#second" class="button scrolly"><img src="images/SCROLL-DOWN.gif" alt=""></a> -->
+    </footer>
+</section>
 
-		<section id="first" class="dark">
-				<header>
-					<h1> Helping Leading</h1>
-					<h1> Brands Scale. </h1>
-					<p>AwesomeAds is a diverse team of best mediabiyers, performance marketing and just a creative guys focused on helping brands scale revenue and bring their unique vision to life.</p>
-					<p>So we may work with any businnes, online or offline - not even matter. Do you want to sell your handmade toys or a Large Hadron Collider? Just ping us and we'll craft tailored growth strategies and help our clients execute them to drive growth.</p>
-				</header>
-				<footer>
-					<!-- <a href="#second" class="button scrolly"><img src="images/SCROLL-DOWN.gif" alt=""></a> -->
-				</footer>
-			</section>
 
-		
+<!-- Second -->
+<section id="second" class="main">
+    <header style="padding: 1em 0 2em 0;">
+        <div class="container">
+            <h2>What are we known for</h2>
+            <p>Our team of experts has many years of experience in mediabuying, online marketing, tv marketing, offline
+                marketing and strategic consulting</p>
+        </div>
+    </header>
 
-		<!-- Second -->
-			<section id="second" class="main">
-				<header>
-					<div class="container">
-						<h2> What are we known for. </h2>
-						<p>Our team of experts has many years of experience in mediabuying, online marketing, tv marketing, offline marketing and strategic consulting</p>
-					</div>
-				</header>
-				<div class="content dark style2">
-					<div class="container">
-						<div class="row">
-							<div class="col-4 col-12-narrow">
-							</div>
-							<div class="col-8 col-12-narrow">
-								 	<div class="information">
-								 		<div class="info">
-								 			<div class="number"><h3>7000+</h3>Active partners around the world</div>
-								 		</div>
-								 		<div class="info">
-								 			<div class="number"><h3>Pay per action</h3>You only pay for the results you REALLY need</div>
-										</div>
-								 		<div class="info">
-								 			<div class="number"><h3>Global traffic</h3>You will get only premium websites users and only BEST ad spots</div>
-								 		</div>
-								 		<div class="info">
-								 			<div class="number"><h3>Fraud protection</h3>Smart anti-fraud algorithms guarantee the high quality of traffic</div>
-								 		</div>
-								 		<div class="info">
-								 			<div class="number"><h3>Individual support</h3>24/7 support will help you optimize your campaigns to reach your goals</div>
-								 		</div>
-								 		<div class="info">
-								 			<div class="number"><h3>Technology</h3>Our smart system lets you start working right away and its extended statistics functionality increases your cost-effectiveness</div>
-								 		</div>
-									</div>
-							</div>
-					</div>
-				</div>
-			</div>
-			</section>
+    <div class="content dark style2">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 col-12-narrow">
+                </div>
+                <div class="col-8 col-12-narrow">
+                    <div class="information">
+                        <div class="info">
+                            <div class="number"><h3>7000+</h3>Active partners around the world</div>
+                        </div>
+                        <div class="info">
+                            <div class="number"><h3>Pay per action</h3>You only pay for the results you REALLY need
+                            </div>
+                        </div>
+                        <div class="info">
+                            <div class="number"><h3>Global traffic</h3>You will get only premium websites users and only
+                                BEST ad spots
+                            </div>
+                        </div>
+                        <div class="info">
+                            <div class="number"><h3>Fraud protection</h3>Smart anti-fraud algorithms guarantee the high
+                                quality of traffic
+                            </div>
+                        </div>
+                        <div class="info">
+                            <div class="number"><h3>Individual support</h3>24/7 support will help you optimize your
+                                campaigns to reach your goals
+                            </div>
+                        </div>
+                        <div class="info">
+                            <div class="number"><h3>Technology</h3>Our smart system lets you start working right away
+                                and its extended statistics functionality increases your cost-effectiveness
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-		<!-- Third -->
-			<section id="third" class="main">
-				<header>
-					<div class="container">
-						<h2>Ready to Join Best Popunder Network?</h2>
-					</div>
-				</header>
-					<div class="container">
-						<div class="parts">
-								 		<div class="part">
-								 			<div class="number"><img src="images/1xbet.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/adsbays@2x.png" alt=""></div>
-										</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/adsimilis.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/avazu.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/billymob.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/cpamatica@2x.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/dafabet.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/eclick.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/favbet@2x.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/gamespipe.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/mobvista@2x.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/msales.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/nextad.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/shakes-mobi@2x.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/ytz.png" alt=""></div>
-								 		</div>
-								 		<div class="part">
-								 			<div class="number"><img src="images/zenads.png" alt=""></div>
-								 		</div>
-									</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+<!-- Third -->
+<section id="third" class="main">
+    <header>
+        <div class="container">
+            <h2>Ready to Join Best Popunder Network?</h2>
+        </div>
+    </header>
+    <div class="container">
+        <div class="parts">
+            <div class="part">
+                <div class="number"><img src="images/1xbet.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/adsbays@2x.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/adsimilis.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/avazu.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/billymob.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/cpamatica@2x.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/dafabet.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/eclick.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/favbet@2x.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/gamespipe.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/mobvista@2x.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/msales.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/nextad.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/shakes-mobi@2x.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/ytz.png" alt=""></div>
+            </div>
+            <div class="part">
+                <div class="number"><img src="images/zenads.png" alt=""></div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+</section>
 
-		
 
-		<!-- Fourth -->
-			<section id="fourth" class="main">
-				<header>
-					<div class="container">
-						<h2>Have Questions? Contact us!</h2>
-						<p> Text us           </p>
-						<p>support@hilltopads.com</p>
-		
-					</div>
-				</header>
-				<div class="content style4 featured">
-					<div class="container medium"> <h3>Send us a message from the site</h3> 
-						<form method="post" action="action.php" id="ajax_form">
-							<div class="row gtr-50">
-								<div class="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name" /></div><br>
-								<div class="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email" /></div><br>
-								<div class="col-12"><textarea name="message" placeholder="Message"></textarea></div>
-								<div class="col-12"><br>
-									<div class="form_result"></div><br>
-									<ul class="actions special">
-										<li><input type="submit" class="button" value="Send Message" /></li>
-									</ul>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
+<!-- Fourth -->
+<section id="fourth" class="main">
 
-			</section>
+    <div class="content  featured">
+<!--        style4-->
+        <div class="container medium">
+            <h3>Have Questions? Contact us!</h3>
+            <form method="post" action="action.php" id="ajax_form">
+                <div class="row gtr-50">
+                    <div class="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name"/></div>
+                    <br>
+                    <div class="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email"/></div>
+                    <br>
+                    <div class="col-12"><textarea name="message" placeholder="Message"></textarea></div>
+                    <div class="col-12"><br>
+                        <div class="form_result"></div>
+                        <br>
+                        <ul class="actions special">
+                            <li><input type="submit" class="button" value="Send Message"/></li>
+                        </ul>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 
-		<!-- Footer -->
-			<section id="footer">
-				<ul class="icons">
-					<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-					<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-				</ul>
-				<div class="copyright">
-					<ul class="menu">
-						<li> </li><li> </li>
-					</ul>
-				</div>
-			</section>
+</section>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-  			<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-			<script src="script.js"></script>
-	</body>
+<!-- Footer -->
+<section id="footer">
+<!--    <ul class="icons">-->
+<!--        <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>-->
+<!--        <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>-->
+<!--        <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>-->
+<!--    </ul>-->
+    <div class="copyright" align="center">
+        <ul class="menu">
+            <li>awesomeads.cloud &copy; 2019-2020</li>
+        </ul>
+    </div>
+</section>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="script.js"></script>
+</body>
 </html>
