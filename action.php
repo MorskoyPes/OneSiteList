1ce6f68ac92bffba93ca42af6b71e0a8956bff9a
Evgeny Netesov <siko.sik.1@gmail.com>
2020-05-05 23:35:45 +0300

    update action

--- action.php
+++ action.php
@@ -1,27 +1,27 @@
 <?php
 $err_message = '';
 
-	if (empty($_POST['name'])) {
-    	$err_message .= "You did not enter a Name"."<br>";
-	}
-	if (empty($_POST['email'])) {
-    	$err_message .= "You did not enter a Mail"."<br>";
-	}
-	if (empty($_POST['message'])) {
-    	$err_message .= "You did not enter a Message"."<br>";
-	}
+if (empty($_POST['name'])) {
+    $err_message .= "You did not enter a Name" . "<br>";
+}
+if (empty($_POST['email'])) {
+    $err_message .= "You did not enter a Mail" . "<br>";
+}
+if (empty($_POST['message'])) {
+    $err_message .= "You did not enter a Message" . "<br>";
+}
 echo $err_message;
 
 if (empty($err_message)) {
 
-$to = $_POST['email'];
-$subject = $_POST['name'];
-$message = $_POST['message'];
-$headers = 'From: kaktyc_real_ii@mail.ru' . "\r\n" .
-    'Reply-To: kaktyc_real_ii@mail.ru' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-mail($to, $subject, $message, $headers);
+    $to      = $_POST['email'];
+    $subject = $_POST['name'];
+    $message = $_POST['message'];
+    $headers = 'From: kaktyc_real_ii@mail.ru' . "\r\n" .
+        'Reply-To: kaktyc_real_ii@mail.ru' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+//mail($to, $subject, $message, $headers);
 
- echo $_POST['name'].", Message sent successfully!";
+    echo $_POST['name'] . ", Message sent successfully!";
 }
 ?>
